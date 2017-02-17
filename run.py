c84c447e9ca2495873eae3866db38df5794c8413
fnbellomo <fnbellomo@gmail.com>
2017-02-17 11:36:01 -0300

    run the telegam bot in new process

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from server.routes import app
+import multiprocessing
 
+from server.routes import app
+from server.directActionBot import create_bot
+
+# Start the direct action telegram bot in a new process
+process = multiprocessing.Process(target=create_bot)
+process.start()
 
 app.run()
